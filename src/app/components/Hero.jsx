d2706acbe6cd1d39b6bb0df9a4835cb1406752f5
weio9134 <weiouyang9134@gmail.com>
2024-02-17 15:09:15 -0500

    Implemented navbar and header

---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,68 @@
+"use client"
+import React from 'react'
+import Image from 'next/image'
+import { TypeAnimation } from 'react-type-animation';
+
+const TextTransition = () => {
+  return (
+    <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed out once, initially
+        'Wei.',
+        1000, // wait 1s before replacing words
+        'a web developer!',
+        1000,
+        'ready to make an impact on your team!',
+        1000
+      ]}
+      wrapper="span"
+      speed={50}
+      repeat={Infinity}
+    />
+  );
+};
+
+const Hero = () => {
+  return (
+    <section> 
+      <div className='grid grid-cols-1 lg:grid-cols-12'>
+
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <div>
+            <h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'>
+              <span className='text-transparent bg-clip-text bg-gradient-to-tr from-gray-500 to-blue-700'> Hello, I'm </span>
+              <br />
+              <TextTransition />
+            </h1>
+          </div>
+
+          <p className='text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6'>
+            Welcome to my website! 
+          </p>
+
+          <div>
+            <button className='btn px-6 py-3 rounded-full w-full sm:w-fit mr-4 bg-white hover:bg-slate-200 text-black'> Recruit Me </button>
+            <button className='btn px-6 py-3 rounded-full w-full sm:w-fit bg-transparent hover:bg-slate-800 text-white border border-white mt-3 '> Download CV </button>
+          </div>
+        </div>
+        
+
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full bg-[#181818] relative w-[250px] h-[250px] lg:w-[400px] lg:h-[400px]">
+            <Image
+              className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
+              src='/images/blorrb.gif'
+              alt='blorrb_temp_pfp'
+              width={300}
+              height={300}
+            />
+          </div>
+          
+        </div>
+
+      </div>
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,53 @@
+"use client"
+import React, { useState } from 'react'
+import Link from 'next/link'
+import { Bars3Icon, XmarkIcon } from '@heroicons/react/24/solid'
+import Menu from './Menu'
+
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <nav className='fixed top-5 left-5 right-5 z-10 bg-[#121212] bg-opacity-100'>
+      <div className='flex flex-wrap items-center justify-between mx-auto px-4 py-2'>
+        <Link href="/" className='text-xl md:text-3xl text-white font-semibold'> 
+          Home 
+        </Link>
+
+        <div className='mobile-menu block md:hidden dropdown dropdown-bottom dropdown-end'>
+          <div tabIndex={0} className="m-1">
+            { !open ? 
+              (
+                <button onClick={() => setOpen(true)} className='btn text-slate-200 flex items-center border rounded border-slate-200 hover:text-white hover:border-white'> 
+                  <Bars3Icon className='h-5 w-5'/> 
+                </button>
+              ) : (
+                <button onClick={() => setOpen(false)} className='btn text-slate-200 flex items-center border rounded border-slate-200 hover:text-white hover:border-white'> 
+                  {/* <XmarkIcon className="h-5 w-5"/>  */}
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                    <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+                  </svg>
+                </button>
+              )
+            }
+            { open ? (
+                <ul tabIndex={0} className="flex dropdown-content z-[1] menu shadow bg-base-100 rounded-box w-52">
+                 <Menu />
+                </ul>
+              ) : (null)
+            }
+          </div>
+        </div>
+
+        <div className="menu hidden md:block md:w-auto" id="navbar">
+          <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0'>
+            <Menu />
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
