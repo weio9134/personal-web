4cd85e95eb47c578e03556668462f318c2762b1e
weio9134 <weiouyang9134@gmail.com>
2024-02-21 14:20:12 -0500

    Changed some words

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -13,7 +13,7 @@ const sections = [
         <li> Java </li>
         <li> Python </li>
         <li> React </li>
-        <li> NextJS </li>
+        <li> Next.js </li>
         <li> Tailwind CSS </li>
         <li> Git </li>
       </ul>
@@ -48,7 +48,7 @@ const AboutSection = () => {
 
   return (
     <section className="text-white" id="about">
-        <div className="md:grid md:grid-cols-2 items-center px-4 md:gap-10 sm:py-16 xl:px-16">
+        <div className="md:grid md:grid-cols-2 items-center px-4 md:gap-5 sm:py-16 xl:px-16">
           <h2 className="md:col-span-2 text-4xl font-bold text-white mb-4"> About Me </h2>
           <div className="w-full h-full mt-0 pt-0">
             <Shapes />
@@ -56,8 +56,8 @@ const AboutSection = () => {
           <div className="w-full h-full md:col-start-2">
             <div className="text-left flex flex-col h-full place-self-center">
               <p className="text-base lg:text-lg">
-                I'm a current Cornell student with a passion for web development and ML.
-                I'm a quick learner and always looking for ways to expand my knowledge and skill set.
+                I am currently studying at Cornell University and I have a strong interest in web development and machine learning. 
+                I'm a quick learner and constantly seeking new opportunities to expand my knowledge and skills.
                 I am a team player, always excited to work with others to create amazing applications!
               </p>
               <div className="flex flex-row justify-start mt-8">
@@ -71,7 +71,7 @@ const AboutSection = () => {
                 ))}
               </div>
               <div className="mt-8">
-                {sections.find((t) => t.id === tab).content}
+                {sections?.find((t) => t.id === tab).content}
               </div>
             </div>
           </div>
--- src/app/page.js
+++ src/app/page.js
@@ -8,8 +8,7 @@ import { Fade } from 'react-awesome-reveal';
 
 export default function Home() {
   return (
-    // <main className='lex min-h-screen flex-col bg-[#121212]'>
-    <main className='lex min-h-screen flex-col bg-gradient-to-tr from-[#070F2B] to-[#535C91]'>
+    <main className='flex min-h-screen flex-col bg-gradient-to-tr from-[#070F2B] to-[#535C91]'>
       <div className="container mx-auto px-12 py-4">
         <Navbar />
         <div className="container mt-24 mx-auto px-12 py-4 space-y-10">
